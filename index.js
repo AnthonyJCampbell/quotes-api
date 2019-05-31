53261fe0fee33656cd500e047bee54c8bba00693
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-31 19:45:41 +0200

    Dotenv

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// require('dotenv').config();
+require('dotenv').config();
 
 const server = require('./server.js');
 
-// const port = process.env.PORT || 7000;
-const port =  7777;
+const port = process.env.PORT || 7777;
 server.listen(port, () => console.log(
   `\n** Running on port ${port} **\n`
 ));
