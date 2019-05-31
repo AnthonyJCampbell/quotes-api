5f3c329d4fb44abca3eba84223adea09e58f1dc9
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-31 19:41:18 +0200

    Restructured knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,39 +5,40 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/neolangDevelopment.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   },
 
-  staging: {
-    client: 'postgresql',
+  testing: {
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/neolangTest.db3'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
     }
   },
+
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
     }
   }
 
