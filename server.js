17c943ec23a7566990781cddd6eb235f603eb45a
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-31 20:04:04 +0200

    Adding JSON object

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const helmet = require('helmet');
 
 const server = express();
 
+const quotes = require('./quotes')
+
 server.use(helmet())
 server.use(express.json());
 server.use(cors())
 
 server.get('/', (req, res) => {
-  return res.status(200).json({"message": "Server's alive!"})
+  return res.status(200).json({"quotes": quotes})
 })
 
 module.exports = server;
