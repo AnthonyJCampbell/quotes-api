e6d9ad3a0e092973099cecdd597e936ac256d771
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-31 19:51:31 +0200

    Removing CORS as an experiment

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors')
 const helmet = require('helmet');
 
 const server = express();
 
 server.use(helmet())
 server.use(express.json());
-server.use(cors())
 
 server.get('/', (req, res) => {
   return res.status(200).json({"message": "Server's alive!"})
